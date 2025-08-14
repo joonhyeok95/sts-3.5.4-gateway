0be0375416be8188cb1147ab54309a9906eda2b3
joonhyeok95 <dkttkemf@gmail.com>
2025-08-14 11:16:44 +0900

    코드 미사용삭제, 패키지명 정리

--- src/main/java/com/metanet/study/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/metanet/study/global/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.metanet.study.global.component;
+package com.metanet.study.global.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
